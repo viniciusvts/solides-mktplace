6c16abd285598155e1164a053193b2c0711efed0
Vinicius de Santana <vinicius.vts@gmail.com>
2020-06-29 11:04:09 -0300

    add custom certificados

---
+++ wp-content/themes/themeplace-child/custom_posts/certificados.php
@@ -0,0 +1,53 @@
+<?php
+function custom_certificados() {
+	$labels = array(
+		'name'                  => _x( 'Certificados', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Certificados', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Certificados', 'text_domain' ),
+		'name_admin_bar'        => __( 'Certificados', 'text_domain' ),
+		'archives'              => __( 'Certificados Arquivados', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'Todos os certificados', 'text_domain' ),
+		'add_new_item'          => __( 'Adicionar novo certificado', 'text_domain' ),
+		'add_new'               => __( 'Adicionar novo', 'text_domain' ),
+		'new_item'              => __( 'Novo certificado', 'text_domain' ),
+		'edit_item'             => __( 'Editar certificado', 'text_domain' ),
+		'update_item'           => __( 'Atualizar certificado', 'text_domain' ),
+		'view_item'             => __( 'Ver certificado', 'text_domain' ),
+		'search_items'          => __( 'Buscar certificado', 'text_domain' ),
+		'not_found'             => __( 'Nenhum cadastrado', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Nenhum na lixeira', 'text_domain' ),
+		'featured_image'        => __( 'Imagem destaque', 'text_domain' ),
+		'set_featured_image'    => __( 'Definir imagem', 'text_domain' ),
+		'remove_featured_image' => __( 'Remover imagem', 'text_domain' ),
+		'use_featured_image'    => __( 'Usar imagem certificado', 'text_domain' ),
+		'insert_into_item'      => __( 'Inserir no certificado', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Subir para certificado', 'text_domain' ),
+		'items_list'            => __( 'Lista de certificado', 'text_domain' ),
+		'items_list_navigation' => __( 'Navegar pelos certificado', 'text_domain' ),
+		'filter_items_list'     => __( 'Filtrar certificado', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Certificados', 'text_domain' ),
+		'description'           => __( 'Cadastrar certificado', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title' ),
+		'taxonomies'            => array( 'categoria_certificados' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'show_in_rest'          => true,
+		'menu_position'         => 30,
+		'menu_icon'				=> 'dashicons-format-aside',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => false,
+		'has_archive'           => false,		
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'certificados', $args );
+}
+add_action( 'init', 'custom_certificados', 0 );
