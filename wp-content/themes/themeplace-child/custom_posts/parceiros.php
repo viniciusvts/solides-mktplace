938fb2423567e8e427148b0f166e2e6cab2a26a6
Vinicius de Santana <vinicius.vts@gmail.com>
2020-07-06 22:45:43 -0300

    crete custom post "parceiros"

---
+++ wp-content/themes/themeplace-child/custom_posts/parceiros.php
@@ -0,0 +1,53 @@
+<?php
+function custom_parceiros() {
+	$labels = array(
+		'name'                  => _x( 'Parceiros', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Parceiros', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Parceiros', 'text_domain' ),
+		'name_admin_bar'        => __( 'Parceiros', 'text_domain' ),
+		'archives'              => __( 'Parceiros Arquivados', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'Todos os parceiros', 'text_domain' ),
+		'add_new_item'          => __( 'Adicionar novo parceiro', 'text_domain' ),
+		'add_new'               => __( 'Adicionar novo', 'text_domain' ),
+		'new_item'              => __( 'Novo parceiro', 'text_domain' ),
+		'edit_item'             => __( 'Editar parceiro', 'text_domain' ),
+		'update_item'           => __( 'Atualizar parceiro', 'text_domain' ),
+		'view_item'             => __( 'Ver parceiro', 'text_domain' ),
+		'search_items'          => __( 'Buscar parceiro', 'text_domain' ),
+		'not_found'             => __( 'Nenhum cadastrado', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Nenhum na lixeira', 'text_domain' ),
+		'featured_image'        => __( 'Imagem destaque', 'text_domain' ),
+		'set_featured_image'    => __( 'Definir imagem', 'text_domain' ),
+		'remove_featured_image' => __( 'Remover imagem', 'text_domain' ),
+		'use_featured_image'    => __( 'Usar imagem parceiro', 'text_domain' ),
+		'insert_into_item'      => __( 'Inserir no parceiro', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Subir para parceiro', 'text_domain' ),
+		'items_list'            => __( 'Lista de parceiro', 'text_domain' ),
+		'items_list_navigation' => __( 'Navegar pelos parceiro', 'text_domain' ),
+		'filter_items_list'     => __( 'Filtrar parceiro', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Parceiros', 'text_domain' ),
+		'description'           => __( 'Cadastrar parceiro', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title' ),
+		'taxonomies'            => array( 'categoria_parceiros' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'show_in_rest'          => true,
+		'menu_position'         => 30,
+		'menu_icon'				=> 'dashicons-groups',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'parceiros', $args );
+}
+add_action( 'init', 'custom_parceiros', 0 );
