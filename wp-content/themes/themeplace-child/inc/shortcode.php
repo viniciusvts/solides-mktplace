8d95e2430480d1c122618a40a803d48653799a5f
Vinicius de Santana <vinicius.vts@gmail.com>
2020-07-07 11:26:16 -0300

    fiix path to archive

--- wp-content/themes/themeplace-child/inc/shortcode.php
+++ wp-content/themes/themeplace-child/inc/shortcode.php
@@ -10,7 +10,7 @@ function cts_shortcode_add( $atts ) {
 	$name = shortcode_atts( array('short' => null), $atts );//ou o array ou o atributo, se houver
     if(!$name){return null;}//se não tem atributo, não tem shortcode
     $archive = $name['short'].'.php';
-    include_once 'shortcodes\/'.$archive;
+    include_once 'shortcodes/'.$archive;
     // return returnit();
     return;
 }
